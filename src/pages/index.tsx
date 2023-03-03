0af400597539e8f22f0bd40f67785ae7db1df6be
ponciano.raf <ponciano.raf@gmail.com>
2023-03-03 18:35:35 -0300

    atualizando a imagem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
 import Image from 'next/image'
 import TopMenu from '@/components/TopMenu'
 
-import rect7 from '../assets/rectangle7.svg';
-import rect2 from '../assets/rectangle2.png';
-import rect3 from '../assets/rectangle3.png';
-import rect4 from '../assets/rectangle4.png';
-import rect5 from '../assets/rectangle5.png';
-import rect6 from '../assets/rectangle6.png';
+import banner from '../assets/test_cinepolis.png';
 
 export default function Home() {
   return (
@@ -20,20 +15,7 @@ export default function Home() {
             Happy to see you here!  
           </span>
         </div>
-        <div className='bg-first-background h-3/5 flex justify-between'>
-          <div className='flex flex-col gap-4 items-center justify-center pl-[10%]'>
-            <span className='text-6xl text-white'>Cinépolis</span>
-            <span className='text-4xl text-white'>UX Design / UI Design</span>
-          </div>
-          <div className='flex relative pr-[5%]'>
-            <Image width={400} alt='cellphone' src={rect7} />
-            {/* <Image width={300} height={300} alt='cellphone' src={rect2} /> */}
-            {/* <Image width={300} height={300} alt='cellphone' src={rect4} /> */}
-            {/* <Image width={300} height={300} alt='cellphone' src={rect3} /> */}
-            {/* <Image width={200} alt='cellphone' src={rect5} />
-            <Image width={300} height={300} alt='cellphone' src={rect6} /> */}
-          </div>
-        </div>
+          <Image className='w-full' alt='banner-portfolio' src={banner} />
       </div>
     </>
   )
